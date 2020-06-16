16af10fd8722612fd25134bca6cffb6d455f2116
ucrengineer <52877259+ucrengineer@users.noreply.github.com>
2020-06-16 11:46:06 -0700

    Add files via upload

---
+++ bfs_2.py
@@ -0,0 +1,61 @@
+""" This program will implement the BFS algorithmn"""
+import queue
+import numpy as np
+import pandas as pd
+from Table import *
+
+
+def bfs_algorithm(Graph,S,G):
+    #create empy list of variables
+    queue = []
+    parent = []
+    node = []
+    queue.append(S)
+    node.append(S)
+    #for each node in graph
+    for each in Graph:
+        #parent(node) = none
+        parent.append('None')
+    #parent(start node) = self
+    parent[S] = 'Self'
+    while len(queue) != 0:
+        #LIFO function for queue
+        #Get the nodes it touches
+        v = Graph[queue.pop()]
+        #Get the node
+        j = node.pop()
+        i = 0
+
+        #For each node
+        for each in v:
+            #If it is touching the node and has not touched before
+            if each == 1 and parent[i] == 'None':
+                #Parent of that node is v
+                parent[i] = j
+                #Add that node its touching to queue
+                queue.append(i)
+                node.append(i)
+            #If the node that is popped the goal    
+            elif j == G:
+                #Want map of graph so continue
+                #If just want path add a "return"
+                """print('success')"""
+            i = i + 1
+    print('Parent Vector: %s' %parent)
+    extract_path(G,parent)
+
+    
+def extract_path(G,parent):
+    P = []
+    P.append(G)
+    u = G
+    while parent[u] != 'Self':
+        u = parent[u]
+        P = [u] + P
+    print('Path: %s' %P)
+        
+
+S = 0
+G = 31
+bfs_algorithm(data,S,G)
+print('Start: %s Goal: %s' %(S,G))
